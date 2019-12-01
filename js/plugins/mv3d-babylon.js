a58dbc1529ade3d2bbebd7989ddfceca6167ce13
Unknown <dreadwing93@ymail.com>
2019-12-01 11:46:19 -0500

    increment version

--- js/plugins/mv3d-babylon.js
+++ js/plugins/mv3d-babylon.js
@@ -1,7 +1,7 @@
 /*:
 @plugindesc 3D rendering in RPG Maker MV with babylon.js
 @author Dread/Nyanak
-@version 0.3.3
+@version 0.3.5
 @help
 
 If you are making a game with this plugin, please consider supporting my
