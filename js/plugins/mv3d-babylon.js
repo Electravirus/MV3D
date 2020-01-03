fd8cfe2f2640da88eba42750991c3bd5bd0f0b78
Unknown <dreadwing93@ymail.com>
2020-01-03 17:12:48 -0500

    0.4.1

--- js/plugins/mv3d-babylon.js
+++ js/plugins/mv3d-babylon.js
@@ -1,7 +1,7 @@
 /*:
 @plugindesc 3D rendering in RPG Maker MV with babylon.js
 @author Dread/Nyanak
-@version 0.4
+@version 0.4.1
 @help
 
 If you are making a game with this plugin, please consider supporting my
