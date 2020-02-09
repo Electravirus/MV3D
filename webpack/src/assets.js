f9a197d1574213c1155e7c618309445965e3ed19
Dread-chan <Dread-chan@users.noreply.github.com>
2020-02-09 14:45:49 -0500

    use DynamicTexture for encrypted images
    also fixed the blink bug

--- webpack/src/assets.js
+++ webpack/src/assets.js
@@ -8,72 +8,104 @@ Object.assign(mv3d,{
 	textureCache:{},
 	materialCache:{},
 
-	createTexture(url){
+	createTextureSync(url){
 		if(Decrypter.hasEncryptedImages){
 			const bitmap = ImageManager.loadNormalBitmap(url);
-			const texture = new BABYLON.Texture(null,mv3d.scene,!mv3d.MIPMAP,false,BABYLON.Texture.NEAREST_SAMPLINGMODE,null,null,bitmap._image);
+			//const texture = new BABYLON.Texture(null,mv3d.scene,!mv3d.MIPMAP,false,BABYLON.Texture.NEAREST_SAMPLINGMODE,null,null,bitmap._image);
+			const texture = new BABYLON.DynamicTexture('dt',bitmap.canvas,!mv3d.MIPMAP,BABYLON.Texture.NEAREST_SAMPLINGMODE);
+			bitmap.addLoadListener(()=>{
+				texture._texture.baseWidth=texture._texture.width=texture._canvas.width;
+				texture._texture.baseHeight=texture._texture.height=texture._canvas.height;
+				texture.update();
+			});
 			return texture;
 		}
 		return new Texture(url,this.scene,!mv3d.MIPMAP);
 	},
 
-	getCachedTilesetTexture(setN,animX=0,animY=0){
+	createTexture(url){return new Promise((resolve,reject)=>{
+		const bitmap = ImageManager.loadNormalBitmap(url);
+		if(Decrypter.hasEncryptedImages){
+			bitmap.addLoadListener(()=>{
+				const texture = new BABYLON.DynamicTexture('dt',{width:bitmap.width,height:bitmap.height},!mv3d.MIPMAP,BABYLON.Texture.NEAREST_SAMPLINGMODE);
+				texture.updateSamplingMode(1);
+				texture.getContext().drawImage(bitmap.canvas,0,0);
+				texture.update();
+				resolve(texture);
+			});
+		}else{
+			const texture = new BABYLON.Texture(bitmap._image.src,mv3d.scene,!mv3d.MIPMAP,false,BABYLON.Texture.NEAREST_SAMPLINGMODE);
+			resolve(texture);
+		}
+
+	})},
+
+	waitTextureLoaded(texture){return new Promise((resolve,reject)=>{
+		if(texture.isReady()){ resolve(); }
+		texture.onLoadObservable.addOnce(()=>{
+			resolve();
+		});
+	})},
+
+	async getCachedTilesetTexture(setN,animX=0,animY=0){
 		const key = `TS:${setN}|${animX},${animY}`;
 		if(key in this.textureCache){
 			return this.textureCache[key];
 		}
 		const tsName = $gameMap.tileset().tilesetNames[setN];
 		if(!tsName){
-			return this.getErrorTexture();
+			return await this.getErrorTexture();
 		}
 		const textureSrc=`img/tilesets/${tsName}.png`;
-		const texture = this.createTexture(textureSrc);
+		const texture = await this.createTexture(textureSrc);
 		texture.hasAlpha=true;
-		texture.onLoadObservable.addOnce(()=>{
-			if(this.textureCache[key]!==texture){ return; }
-			texture.updateSamplingMode(1);
-			if(animX||animY){
-				const { width, height } = texture.getBaseSize();
-				texture.frameData={x:0,y:0,w:width,h:height};
-				texture.animX = animX;
-				texture.animY = animY;
-				this.animatedTextures.push(texture);
-			}
-		});
 		this.textureCache[key]=texture;
-		return texture;
-	},
-	getCachedTilesetTextureAsync(setN,animX=0,animY=0){return new Promise((resolve,reject)=>{
-		const texture = this.getCachedTilesetTexture(setN,animX,animY);
-		//if(texture.isError){ return void reject(); }
-		if(texture.isReady()){
-			resolve(texture);
-		}else{
-			texture.onLoadObservable.addOnce(()=>{ resolve(texture); });
-		}
-	})},
 
-	getErrorTexture(){
+		await this.waitTextureLoaded(texture);
+
+		if(this.textureCache[key]!==texture){ return await this.getErrorTexture(); }
+		texture.updateSamplingMode(1);
+		if(animX||animY){
+			const { width, height } = texture.getBaseSize();
+			texture.frameData={x:0,y:0,w:width,h:height};
+			texture.animX = animX;
+			texture.animY = animY;
+			this.animatedTextures.push(texture);
+		}
+		return texture;
+		
+	},
+
+	async getErrorTexture(){
 		if(this.errorTexture){ return this.errorTexture; }
-		this.errorTexture = this.createTexture(`${mv3d.MV3D_FOLDER}/errorTexture.png`);
+		this.errorTexture = await this.createTexture(`${mv3d.MV3D_FOLDER}/errorTexture.png`);
 		this.errorTexture.isError=true;
 		return this.errorTexture;
 	},
 
-	getBushAlphaTexture(){
+	async getBushAlphaTexture(){
 		if(this.bushAlphaTexture){ return this.bushAlphaTexture; }
-		this.bushAlphaTexture = this.createTexture(`${mv3d.MV3D_FOLDER}/bushAlpha.png`);
+		this.getBushAlphaTexture.getting=true;
+		this.bushAlphaTexture = await this.createTexture(`${mv3d.MV3D_FOLDER}/bushAlpha.png`);
 		this.bushAlphaTexture.getAlphaFromRGB=true;
+		this.getBushAlphaTexture.getting=false;
 		return this.bushAlphaTexture;
 	},
+	getBushAlphaTextureSync(){
+		if(this.bushAlphaTexture){ return this.bushAlphaTexture; }
+		if(!this.getBushAlphaTexture.getting){
+			this.getBushAlphaTexture();
+		}
+		return null;
+	},
 
-	getCachedTilesetMaterial(setN,animX=0,animY=0,options={}){
+	async getCachedTilesetMaterial(setN,animX=0,animY=0,options={}){
 		this.processMaterialOptions(options);
 		const key = `TS:${setN}|${animX},${animY}|${this.getExtraBit(options)}`;
 		if(key in this.materialCache){
 			return this.materialCache[key];
 		}
-		const texture = this.getCachedTilesetTexture(setN,animX,animY);
+		const texture = await this.getCachedTilesetTexture(setN,animX,animY);
 		const material = new StandardMaterial(key, this.scene);
 		material.diffuseTexture=texture;
 		if(options.transparent){
@@ -89,22 +121,12 @@ Object.assign(mv3d,{
 		return material;
 	},
 
-	getCachedTilesetMaterialAsync(setN,animX=0,animY=0,options={}){return new Promise((resolve,reject)=>{
-		const material = this.getCachedTilesetMaterial(setN,animX,animY,options);
-		const texture = material.diffuseTexture;
-		if(texture.isReady()){
-			resolve(material);
-		}else{
-			texture.onLoadObservable.addOnce(()=>{ resolve(material); });
-		}
-	})},
-
 	async getCachedTilesetMaterialForTile(tileConf,side){
 		const setN = mv3d.getSetNumber(tileConf[`${side}_id`]);
 		const options = mv3d.getMaterialOptions(tileConf,side);
 		const animData = mv3d.getTileAnimationData(tileConf,side);
 		//console.log(options);
-		return await mv3d.getCachedTilesetMaterialAsync(setN,animData.animX,animData.animY,options);
+		return await mv3d.getCachedTilesetMaterial(setN,animData.animX,animData.animY,options);
 	},
 
 	processMaterialOptions(options){
